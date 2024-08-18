5fe63c51128e3910f4c442c86000c6a41fa7b676
VijayRaj <vijay@plutos.one>
2024-08-18 18:08:43 +0530

    completed code

--- src/service.js
+++ src/service.js
@@ -2,14 +2,14 @@ import { toast } from "react-toastify";
 import { privateRequest } from "./configs/RequestMethod";
 import Cookies from 'js-cookie';
 import { changeUserData, login, logout } from "./features/auth-slice";
-const jwtToken = Cookies.get('jwtToken');
+
 
 export const loginPageImg = "https://pngfre.com/wp-content/uploads/hanuman-36-281x300.png"
 const pdfUrl = "https://www.hindutemplealbany.org/wp-content/uploads/2016/08/Sri_Hanuman_Chalisa_Hindi.pdf"
-const dhyaanPoster= "https://cdn.exoticindia.com/images/products/original/books/gpa229.jpg"
-const coverImage="https://m.media-amazon.com/images/I/715smRhK8JL._AC_UF1000,1000_QL80_.jpg"
+const dhyaanPoster = "https://cdn.exoticindia.com/images/products/original/books/gpa229.jpg"
+const coverImage = "https://m.media-amazon.com/images/I/715smRhK8JL._AC_UF1000,1000_QL80_.jpg"
 
-export const brandImg ="https://w7.pngwing.com/pngs/898/783/png-transparent-om-namaste-hinduism-symbol-yoga-hindu-gods-text-trademark-logo.png"
+export const brandImg = "https://w7.pngwing.com/pngs/898/783/png-transparent-om-namaste-hinduism-symbol-yoga-hindu-gods-text-trademark-logo.png"
 
 
 export const USERLOGIN = async (formData, navigate, dispatch) => {
@@ -21,7 +21,7 @@ export const USERLOGIN = async (formData, navigate, dispatch) => {
     navigate('/')
 }
 
-export const USERLOGOUT =( navigate, dispatch) => {
+export const USERLOGOUT = (navigate, dispatch) => {
     dispatch(logout());
     Cookies.remove('jwtToken');
     toast.success('Logged Out Successfully');
@@ -29,39 +29,43 @@ export const USERLOGOUT =( navigate, dispatch) => {
 }
 
 export const UPDATEADMINDETAILS = async (formData, dispatch) => {
-    const response = await privateRequest.put('/profile', formData,{
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.put('/profile', formData, {
         headers: {
-        Authorization: `Bearer ${jwtToken}`,
+            Authorization: `Bearer ${jwtToken}`,
         }
-  });
+    });
     dispatch(login(response?.data));
     toast.success(response.data.message)
 }
 
 export const GETPROFILE = async () => {
-    const response = await privateRequest.get('/profile',{
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.get('/profile', {
         headers: {
-        Authorization: `Bearer ${jwtToken}`,
+            Authorization: `Bearer ${jwtToken}`,
         }
-  });
+    });
     return response
 }
 
-export const CHANGEPASSWORD = async (formData, navigate,dispatch) => {
-    console.log("jwtToken",jwtToken)
-    const response = await privateRequest.put('/change_password', formData,{
-              headers: {
-              Authorization: `Bearer ${jwtToken}`,
+export const CHANGEPASSWORD = async (formData, navigate, dispatch) => {
+    const jwtToken = Cookies.get('jwtToken');
+    console.log("jwtToken", jwtToken)
+    const response = await privateRequest.put('/change_password', formData, {
+        headers: {
+            Authorization: `Bearer ${jwtToken}`,
         },
     });
     dispatch(logout(response?.data));
     toast.success(response.data.message)
     localStorage.removeItem('user');
     localStorage.removeItem('isAuthenticated');
-    navigate('/login'); 
+    navigate('/login');
 }
 
 export const GETDASHBOARDDATA = async () => {
+    const jwtToken = Cookies.get('jwtToken');
     const response = await privateRequest.get('/dashboard', {
         headers: {
             Authorization: `Bearer ${jwtToken}`,
@@ -71,6 +75,7 @@ export const GETDASHBOARDDATA = async () => {
 }
 
 export const ADDBOOK = async (formData) => {
+    const jwtToken = Cookies.get('jwtToken');
     const response = await privateRequest.post("/book", formData, {
         headers: {
             Authorization: `Bearer ${jwtToken}`,
@@ -81,6 +86,7 @@ export const ADDBOOK = async (formData) => {
 }
 
 export const GETBOOKS = async (searchTerm = "", page = 1, limit = 10) => {
+    const jwtToken = Cookies.get('jwtToken');
     const response = await privateRequest.get(`/all_books`, {
         headers: {
             Authorization: `Bearer ${jwtToken}`,
@@ -95,9 +101,10 @@ export const GETBOOKS = async (searchTerm = "", page = 1, limit = 10) => {
 }
 
 
-export const UPDATEBOOK = async ( id,formData) => {
-    const response = await privateRequest.put(`/book/${id}`,formData,{
-       headers: {
+export const UPDATEBOOK = async (id, formData) => {
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.put(`/book/${id}`, formData, {
+        headers: {
             Authorization: `Bearer ${jwtToken}`,
             'Content-Type': 'multipart/form-data'
         }
@@ -106,8 +113,9 @@ export const UPDATEBOOK = async ( id,formData) => {
 }
 
 export const DELETEBOOK = async (id) => {
-    const response = await privateRequest.delete(`/book/${id}`,{
-       headers: {
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.delete(`/book/${id}`, {
+        headers: {
             Authorization: `Bearer ${jwtToken}`,
         }
     });
@@ -115,6 +123,7 @@ export const DELETEBOOK = async (id) => {
 }
 
 export const ADDDHYAAN = async (formData) => {
+    const jwtToken = Cookies.get('jwtToken');
     const response = await privateRequest.post("/dhyaan", formData, {
         headers: {
             Authorization: `Bearer ${jwtToken}`,
@@ -125,7 +134,8 @@ export const ADDDHYAAN = async (formData) => {
 }
 
 export const GETDHYAAN = async (searchTerm = "", page = 1, limit = 10) => {
-    const response = await privateRequest.get("/all_dhyaans",{
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.get("/all_dhyaans", {
         headers: {
             Authorization: `Bearer ${jwtToken}`,
         },
@@ -138,9 +148,10 @@ export const GETDHYAAN = async (searchTerm = "", page = 1, limit = 10) => {
     return response
 }
 
-export const UPDATEDHYAAN = async ( id,formData) => {
-    const response = await privateRequest.put(`/dhyaan/${id}`,formData,{
-       headers: {
+export const UPDATEDHYAAN = async (id, formData) => {
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.put(`/dhyaan/${id}`, formData, {
+        headers: {
             Authorization: `Bearer ${jwtToken}`,
             'Content-Type': 'multipart/form-data'
         }
@@ -149,17 +160,19 @@ export const UPDATEDHYAAN = async ( id,formData) => {
 }
 
 export const DELETEDHYAAN = async (id) => {
-    const response = await privateRequest.delete(`/dhyaan/${id}`,{
-       headers: {
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.delete(`/dhyaan/${id}`, {
+        headers: {
             Authorization: `Bearer ${jwtToken}`,
         }
     });
     return response
 }
 
-export const GETUSERS = async (page,limit) => {
-    const response = await privateRequest.get(`/getAllUsers/`,{
-       headers: {
+export const GETUSERS = async (page, limit) => {
+    const jwtToken = Cookies.get('jwtToken');
+    const response = await privateRequest.get(`/getAllUsers/`, {
+        headers: {
             Authorization: `Bearer ${jwtToken}`,
         },
         params: {
@@ -171,6 +184,7 @@ export const GETUSERS = async (page,limit) => {
 }
 
 export const FORGOTPASSWORD = async (formData) => {
+
     const response = await privateRequest.post("/forget_password", formData, {
     });
     return response
@@ -182,7 +196,7 @@ export const VERIFYOTP = async (formData) => {
     return response
 }
 
-export const RESETPASSWORD = async (formData,navigate) => {
+export const RESETPASSWORD = async (formData, navigate) => {
     const response = await privateRequest.post("/reset_password", formData, {
     });
     navigate("/login")
